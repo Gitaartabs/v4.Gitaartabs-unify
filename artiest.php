d3ee34dbd5206f2d4e7c4f344fecc263d6b815f5
Gitaartabs <info@gitaartabs.nl>
2014-10-29 15:23:02 +0100

    test

---
+++ artiest.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8"> <![endif]-->  
+<!--[if IE 9]> <html lang="en" class="ie9"> <![endif]-->  
+<!--[if !IE]><!--> <html lang="en"> <!--<![endif]-->  
+<head>
+    <title>Blof Tabs</title>
+
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.ico">
+
+    <!-- CSS Global Compulsory -->
+    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <!-- CSS Implementing Plugins -->
+    <link rel="stylesheet" href="assets/plugins/line-icons/line-icons.css">
+    <link rel="stylesheet" href="assets/plugins/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/plugins/owl-carousel/owl-carousel/owl.carousel.css">    
+
+    <!-- CSS Theme -->    
+    <link rel="stylesheet" href="assets/css/themes/default.css" id="style_color">
+
+    <!-- CSS Customization -->
+    <link rel="stylesheet" href="assets/css/custom.css">
+</head> 
+
+<body>
+<div class="wrapper">
+    <!--=== Header ===-->    
+    <? include 'header.incl.php'; ?> 
+    <!--=== End Header ===-->    
+
+
+
+    <!--=== Breadcrumbs ===-->
+    <div class="breadcrumbs">
+        <div class="container">
+            <h1 class="pull-left">Blof tabs &amp; chords</h1>
+            <ul class="pull-right breadcrumb">
+                <li><a href="gitaar-tabs.php">Home</a></li>
+                <li><a href="artiestenoverzicht.php">Artiesten</a></li>
+                <li><a href="artiestenoverzicht.php">B</a></li>
+                <li class="active">Blof</li>
+            </ul>
+        </div><!--/container-->
+    </div><!--/breadcrumbs-->
+    <!--=== End Breadcrumbs ===-->
+
+    <!--=== Content Part ===-->
+    <div class="container content"> 	
+    	<div class="row portfolio-item margin-bottom-50"> 
+            <!-- Carousel -->
+            <div class="col-md-7">
+                <div class="carousel slide carousel-v1" id="myCarousel">
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img alt="" src="assets/img/artist/blof1.jpg">
+                            <div class="carousel-caption">
+                                <p>Blof (images) indien in de database</p>
+                            </div>
+                        </div>
+                        <div class="item">
+                            <img alt="" src="assets/img/artist/blof2.jpg">
+                            <div class="carousel-caption">
+                                <p>Blof meer images</p>
+                            </div>
+                            </div>
+                    </div>
+                    
+                    <div class="carousel-arrow">
+                        <a data-slide="prev" href="#myCarousel" class="left carousel-control">
+                            <i class="fa fa-angle-left"></i>
+                        </a>
+                        <a data-slide="next" href="#myCarousel" class="right carousel-control">
+                            <i class="fa fa-angle-right"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <!-- End Carousel -->
+
+            <!-- Content Info -->        
+            <div class="col-md-5">
+            	<h2>Blof biografie</h2>
+                <span class="dropcap">B</span><p>lof is opgericht in 1992.Het eerste album Naakt onder de hemel werd in eigen beheer uitgebracht. In 1998 werd de band populair onder het grote publiek met de single Liefs uit Londen.Hierna kon het niet meer stuk met de band.Totdat er een tragisch ongeval zich voordeed. De drummer Chris Gotte overleed in 2001 na een afschuwelijk motorongeluk.
+				<br><br>De band wilde toch door en vond in Norman Bonink een nieuwe drummer. Zij maakte het album blaauwe ruis.Er waren een aantal nummers van het album die het publiek bereikte. Zoals Dichterbij dan ooit,Mooie dag en Blaauwe ruis.</p>
+                <ul class="list-unstyled">
+                	<li><i class="fa fa-user color-green"></i> Blof</li>
+                	<li><i class="fa fa-calendar color-green"></i> 14,2003 February</li>
+                	<li><i class="fa fa-star color-green"></i><i class="fa fa-star color-green"></i><i class="fa fa-star color-green"></i><i class="fa fa-star color-green"></i><i class="fa fa-star-o color-green"></i> </li>
+                </ul>
+            </div>
+            <!-- End Content Info -->        
+        </div><!--/row-->
+
+        
+        <div class="margin-bottom-20 clearfix"></div>    
+
+		 	<!--Basic Table-->
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th>Blof Tabs & Chords</th>
+						<th>Soort </th>
+						<th class="hidden-sm">Album </th>
+						<th class="hidden-sm">Jaar </th>
+						<th class="hidden-sm">Genre </th>
+						<th>Downloads </th>
+						<th class="hidden-sm">Rating</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td><a href="tab.php">Aan de kust</a></td>
+						<td>chords</td>
+						<td class="hidden-sm">Naakt onder de hemel</td>
+						<td class="hidden-sm">2001</td>
+						<td class="hidden-sm">Nederpop</td>
+						<td><em>68.243</em></td>
+						<td><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star-half-o"></i></td>
+					</tr>
+					<tr>
+						<td><a href="tab.php">Aan de kust</a></td>
+						<td>chords</td>
+						<td class="hidden-sm">Naakt onder de hemel</td>
+						<td class="hidden-sm">2001</td>
+						<td class="hidden-sm">Nederpop</td>
+						<td><em>68.243</em></td>
+						<td><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></td>
+					</tr>
+					<tr>
+						<td><a href="tab.php">Aan de kust</a></td>
+						<td>chords</td>
+						<td class="hidden-sm">Naakt onder de hemel</td>
+						<td class="hidden-sm">2001</td>
+						<td class="hidden-sm">Nederpop</td>
+						<td><em>68.243</em></td>
+						<td><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></td>
+					</tr>
+					<tr>
+						<td><a href="tab.php">Aan de kust</a></td>
+						<td>chords</td>
+						<td class="hidden-sm">Naakt onder de hemel</td>
+						<td class="hidden-sm">2001</td>
+						<td class="hidden-sm">Nederpop</td>
+						<td><em>68.243</em></td>
+						<td><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></td>
+					</tr>
+					<tr>
+						<td><a href="tab.php">Aan de kust</a></td>
+						<td>chords</td>
+						<td class="hidden-sm">Naakt onder de hemel</td>
+						<td class="hidden-sm">2001</td>
+						<td class="hidden-sm">Nederpop</td>
+						<td><em>68.243</em></td>
+						<td><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i></td>
+					</tr>
+				</tbody>
+			</table>
+			<!--End Basic Table-->	
+        
+		<!-- box content biografie -->
+		<div class="row margin-bottom-10">
+				<div>
+					<h2>Blof biografie</h2>
+					<span class="dropcap">B</span><p>lof is opgericht in 1992.Het eerste album Naakt onder de hemel werd in eigen beheer uitgebracht. In 1998 werd de band populair onder het grote publiek met de single Liefs uit Londen.Hierna kon het niet meer stuk met de band.Totdat er een tragisch ongeval zich voordeed. De drummer Chris Gotte overleed in 2001 na een afschuwelijk motorongeluk.
+					<br><br>De band wilde toch door en vond in Norman Bonink een nieuwe drummer. Zij maakte het album blaauwe ruis.Er waren een aantal nummers van het album die het publiek bereikte. Zoals Dichterbij dan ooit,Mooie dag en Blaauwe ruis.</p>
+				</div>
+				<div>
+					<h2>Blof bandleden</h2>
+					<p>Peter Slager (bass/zang)<br>
+						Paskal Jakobsen (zang/gitaar}<Br>
+						Bas Kennis (toetsen/zang)<br>
+						Chris Gotte(R.I.P 2001) nieuwe drummer Norman Bonink </p>
+				</div>
+				<div>
+					<h2>Blof aanvullende informatie</h2>
+					<span class="dropcap">H</span><p>ithoteringen en meer aanvullende informatie uit de database. Blof opgericht in 1992.Het eerste album Naakt onder de hemel werd in eigen beheer uitgebracht. In 1998 werd de band populair onder het grote publiek met de single Liefs uit Londen.Hierna kon het niet meer stuk met de band.Totdat er een tragisch ongeval zich voordeed. De drummer Chris Gotte overleed in 2001 na een afschuwelijk motorongeluk.
+					<br><br>De band wilde toch door en vond in Norman Bonink een nieuwe drummer. Zij maakte het album blaauwe ruis.Er waren een aantal nummers van het album die het publiek bereikte. Zoals Dichterbij dan ooit,Mooie dag en Blaauwe ruis.</p>
+				</div>
+				
+		
+		</div>
+		
+		
+		<!-- End box content biografie -->
+
+		
+
+		
+    </div><!--/container-->	 	
+    <!--=== End Content Part ===-->
+
+    <!--=== Footer ===-->
+    <? include 'footer.incl.php'; ?> 
+    <!--=== End Footer ===-->
+
+</div><!--/wrapper-->
+
+<!-- JS Global Compulsory -->           
+<script type="text/javascript" src="assets/plugins/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="assets/plugins/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script> 
+<script type="text/javascript" src="assets/plugins/back-to-top.js"></script>
+<!-- JS Implementing Plugins -->
+<script type="text/javascript" src="assets/plugins/back-to-top.js"></script>           
+<script type="text/javascript" src="assets/plugins/bxslider/jquery.bxslider.js"></script>
+<script type="text/javascript" src="assets/plugins/owl-carousel/owl-carousel/owl.carousel.js"></script>
+<!-- JS Page Level -->           
+<script type="text/javascript" src="assets/js/app.js"></script>
+<script type="text/javascript" src="assets/js/plugins/owl-carousel.js"></script>
+<script type="text/javascript">
+    jQuery(document).ready(function() {
+        App.init();
+        App.initBxSlider();
+        OwlCarousel.initOwlCarousel();        
+    });
+</script>
+<!--[if lt IE 9]>
+    <script src="assets/plugins/respond.js"></script>
+    <script src="assets/plugins/html5shiv.js"></script>
+<![endif]-->
+
+</body>
+</html> 
